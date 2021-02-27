7e1691d0aba1c78d673a97a3ed06684c2c47fa65
arxidamos <dimos33@hotmail.com>
2021-02-27 20:47:23 +0200

    finish main functionality for random record  creation

--- testFile.sh
+++ testFile.sh
@@ -2,90 +2,97 @@
 virusesFile=$1
 countriesFile=$2
 numLines=$3
+duplicatesAllowed=$4
 
-# Produce random number within given limits
-random_number() {
-    local x=0
-    while [ "$x" -le $1 ]
-    do
-        x=$RANDOM
-        let "x %= $2" #Scale $number within $RANGE
-    done
-    echo "$x"
+# Toss a YES/NO coin
+toss_coin() {
+    local x=$(( $RANDOM%2 )) # x can be either 0 or 1
+    if [ "$x" -eq 0 ]
+    then
+        echo "NO"
+    else
+        echo "YES"
+    fi
 }
 
 # Produce unique random numbers within given limits
-num_array=( $( shuf -i 0-9999 -n "$numLines") )
+random_id() {
+    num_array=( $( shuf -i 0-9999 -n "$numLines") )
+    # If duplicates are allowed, up to 2% of IDs will be duplicates
+    if [ $1 -eq 1 ]
+    then
+        local p=`bc <<< "$numLines*0.03/1"`   # floor to nearest integer
+        while [ $p -gt 0 ]
+        do
+            # get random indices in num_array
+            local k=$(( $RANDOM%$numLines ))
+            local j=$(( $RANDOM%$numLines ))
 
-# If duplicates enabled, 2% of IDs becomes duplicates
-p=`bc <<< "$numLines*0.03/1"`
+            # make the one duplicate of the other
+            num_array[$j]=${num_array[$k]}
 
-# local k=0
-    # while [ $k -le $(( "$numLines"-1 )) ]
-    # do
-    #     # echo ${num_array[$i]}
-    #     k=$(( $k + 1 ))
-# done
+            p=$(( p - 1 ))
+        done
+    fi
+}
 
+# Produce random date
+random_date() {
+    # let d=$date+%d
+    local dd=$(( RANDOM%1+2020 ))
+    local mm=$(( RANDOM%12+1 ))
+    local yyyy=$(( RANDOM%28+1 ))
+    d=`date -d "$dd-$mm-$yyyy" '+%d-%m-%Y'`
+    echo "$d"
+}
+# random_date
+# 889 John Papadopoulos Greece 52 COVID-19 YES 27-12-2020 ######
 
-while [ $p -gt 0 ]
-do
-    # get random indices in num_array
-    k=$(random_number 0 "$numLines")
-    j=$(random_number 0 "$numLines")
-
-    # make the one duplicate of the other
-    num_array[$j]=${num_array[$k]}
-
-    p=$(( p - 1 ))
-done
-
-
+random_id $duplicatesAllowed
 i=0
 while [ $i -lt $numLines ]
 do
-    # Create random citizenID ############################################################################################
-    # string_length=$(random_number 0 5)
-    # # string_length=$(random_id)    
-    # tr -dc '0-9' </dev/urandom | head -z -c $string_length  >> "recordsFile.txt"
-    # echo -n ' ' >> "recordsFile.txt"
-    
+    # Create random citizenID
     citizenID=${num_array[$i]}
     echo $citizenID  | tr '\n' ' ' >> "recordsFile.txt"
-    # echo -n ' ' >> "recordsFile.txt"
 
-    # Create random name ############################################################################################
-    string_length=$(random_number 2 13) # tr: remove (-d) all chars from incoming command BUT (-c) the ones in the set
-    tr -dc 'a-zA-Z0-9' </dev/urandom | head -z -c $string_length  >> "recordsFile.txt"  # head -c : obtain first string_length bytes (chars) from /urandom
+    # Create random name
+    name_length=$(( $RANDOM%13+3 ))
+    # tr -dc: remove all chars from incoming command BUT the ones in the set
+    # head -c : keep first <name_length> bytes (chars) from /urandom
+    tr -dc 'a-zA-Z' </dev/urandom | head -z -c $name_length  >> "recordsFile.txt"
     echo -n ' ' >> "recordsFile.txt"
 
-    # Create random surname ############################################################################################
-    string_length=$(random_number 2 13)
-    tr -dc 'a-zA-Z0-9' </dev/urandom | head -z -c $string_length  >> "recordsFile.txt"
+    # Create random surname
+    name_length=$(( $RANDOM%13+3 ))
+    tr -dc 'a-zA-Z' </dev/urandom | head -z -c $name_length  >> "recordsFile.txt"
     echo -n ' ' >> "recordsFile.txt"
 
-    # Select random country ############################################################################################
+    # Select random country
     shuf -n 1 $countriesFile | tr '\n' ' ' >> "recordsFile.txt" 
 
-    # Create random age ############################################################################################
-
-    age=$(random_number 1 120)
+    # Create random age
+    age=$(( $RANDOM%120+1 ))
     printf "%d " "$age" >> "recordsFile.txt"
 
-    # Select random virus ############################################################################################
-    # shuf -n 1 $virusesFile | tr '\n' ' ' >> "recordsFile.txt"
-    # printf "\n" >> "recordsFile.txt"
-    shuf -n 1 $virusesFile >> "recordsFile.txt"
+    # Select random virus
+    shuf -n 1 $virusesFile | tr '\n' ' '>> "recordsFile.txt"
+
+    # Randomly choose YES/NO
+    vaccined=$(toss_coin)
+    printf "%s" "$vaccined" >> "recordsFile.txt"
+
+    # Create random date, if current dude is vaccined
+    if [[ $vaccined == "YES" ]]
+    then
+        date=$(random_date)
+        printf " %s \n" "$date" >> "recordsFile.txt"
+    else
+        printf " \n" >> "recordsFile.txt"
+    fi
+
     i=$(( $i + 1 ))
 done
 
 # remove last blank line
 truncate -s -1 recordsFile.txt
-
-# echo ${num_array[@]}
-#  local i=0
-#     while [ $i -le $(( "$numLines"-1 )) ]
-#     do
-#         # echo ${num_array[$i]}
-#         i=$(( $i + 1 ))
-#     done
